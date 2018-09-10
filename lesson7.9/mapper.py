5e890ec2166bd2a969c3cedea88a77f887b77ca5
Jeroen Smienk <jeroensmienk@icloud.com>
2018-09-10 11:16:49 +0200

    Implemented mapreduce 7.9.

--- lesson7.9/mapper.py
+++ lesson7.9/mapper.py
@@ -4,10 +4,25 @@
 import sys
 from datetime import datetime
 
-# Outputs key: WEEKDAY     SALES
+# Outputs key: WEEKDAY    value: SALES
 def mapper():
     for line in sys.stdin:
 
+        row = line.strip().split('\t')
+        if len(row) < 5:
+            continue
+
+        # date
+        date = row[0]
+        
+        # sales
+        sales = row[4]
+        if not isinstance(sales, float)
+            continue
+
+        # find weekday
         weekday = datetime.strptime(date, "%Y-%m-%d").weekday()
+
+        print('{0}\t{1}'.format(weekday, sales))
 
 mapper()
--- lesson7.9/reducer.py
+++ lesson7.9/reducer.py
@@ -4,6 +4,45 @@
 import sys
 
 def reducer():
-    # CODE HERE
+
+    prevWeekday = None
+    currWeekday = None
+    currWeekdaySales = []
+
+    for line in sys.stdin:
+
+        data = line.strip().split('\t')
+        if len(data) != 2:
+            continue
+
+        # weekday
+        currWeekday = data[0]
+        # sales
+        sales = data[1]
+
+        # If current weekday does not equal previous weekday
+        if prevWeekday and prevWeekday != currWeekday:
+
+            # Calc mean sales
+            mean = sum(currWeekdaySales) / len(currWeekdaySales)
+
+            # Print the previous weekday and its mean sales
+            print("{0}\t{1}".format(prevWeekday, mean))
+
+            # Reset vars
+            currWeekdaySales = []
+
+            # New current weekday
+            prevWeekday = currWeekday
+
+        # Add sales to total sales list
+        currWeekdaySales.append(sales)
+
+        # Set the current weekday as the previous weekday for next iteration
+        prevWeekday = currWeekday
+
+    # Print the final weekday and its mean sales
+    mean = sum(currWeekdaySales) / len(currWeekdaySales)
+    print("{0}\t{1}".format(prevWeekday, mean))
 
 reducer()

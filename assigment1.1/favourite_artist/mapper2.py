4fe4f684197745b2d41dba19d597eeef2931fd15
Jeroen Smienk <jeroensmienk@icloud.com>
2018-09-13 14:36:34 +0200

    started round 3 mapreduce.

--- assigment1.1/favourite_artist/mapper2.py
+++ assigment1.1/favourite_artist/mapper2.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 """mapper.py"""
 
+# Mapper prints track_id, (artist), (user_id, first_name, last_name, listen_count)
+
 import sys
 
 # combine result round 1 with tracks to find the artists for every song the user listened to.
 def mapper():
-    for line in sys.stdin: # user_id, first_name, last_name, track_id, listen_count
+    for line in sys.stdin:
         data = line.strip().split(',')
 
         track_id = None
@@ -19,12 +21,14 @@ def mapper():
         if len(data) == 4:
             track_id = data[0]
             artist = data[1]
+            # title not important
 
             # Skip header line
             if track_id == 'track_id':
                 continue
 
         elif len(data) == 5: # Read input from the first mapper
+            # user_id, first_name, last_name, track_id, listen_count
             track_id = data[3]
             user_id = data[0]
             first_name = data[1]
@@ -32,8 +36,7 @@ def mapper():
             listen_count = data[4]
         else:
             continue
-        
-        # mapper prints track_id, (artist), (user_id, first_name, last_name, listen_count)
+
         print('{0},{1},{2},{3},{4},{5}'.format(track_id, artist, user_id, first_name, last_name, listen_count))
 
 
--- assigment1.1/favourite_artist/mapper3.py
+++ assigment1.1/favourite_artist/mapper3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """mapper.py"""
 
+# Input is track_id, artist, user_id, first_name, last_name, listen_count
 # Mapper prints user_id, first_name, last_name, track_id, artist, listen_count
 
 import sys
@@ -8,5 +9,10 @@ import sys
 def mapper():
     for line in sys.stdin:
         data = line.strip().split(',')
+
+        if len(data) != 6:
+            continue
+
+        
 
 mapper()

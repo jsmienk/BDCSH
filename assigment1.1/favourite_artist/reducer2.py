c876e3814001cee5af3abeebfcca7fceb4f905b2
MarnickvdA <marnick@vdarend.com>
2018-09-13 15:00:37 +0200

    Redesigned reducer2.py

--- assigment1.1/favourite_artist/reducer2.py
+++ assigment1.1/favourite_artist/reducer2.py
@@ -11,9 +11,8 @@ def reducer():
     prev_track = None
     curr_track = None
     curr_artist = None
-    curr_user_id = None
-    curr_user_first_name = None
-    curr_user_last_name = None
+
+    curr_track_listeners = []
 
     # Input comes from STDIN: track_id, artist, user_id, first_name, last_name, listen_count
     for line in sys.stdin:
@@ -28,40 +27,31 @@ def reducer():
         if not listen_count.isdigit():
             continue
 
+        # Save the artist of the current track_id
         if curr_artist == None and artist != '-':
             curr_artist = artist
 
-        if curr_user_id == None and user_id != '-':
-            curr_user_id = user_id
-
-        if curr_user_first_name == None and first_name != '-':
-            curr_user_first_name = first_name
-
-        if curr_user_last_name == None and last_name != '-':
-            curr_user_last_name = last_name
+        # Append a user to the listeners list
+        if user_id != '-':
+            curr_track_listeners.append([user_id, first_name, last_name, listen_count])
 
         # If current track_id does not equal previous track_id
         if prev_track and prev_track != curr_track:
 
-            # Print the previous user's playhistory
-            print_result(prev_track, curr_artist, curr_user_id, curr_user_first_name, curr_user_last_name, curr_track_count)
+            # Print the previous track's listeners
+            for listener in curr_track_listeners:
+                print_result(prev_track, curr_artist, listener[0], listener[1], listener[2], listener[3])
 
             # Reset variables
-            curr_track_count = 0
-
             curr_artist = None
-            curr_user_id = None
-            curr_user_first_name = None
-            curr_user_last_name = None
-
-        # Increase the count
-        curr_track_count += int(listen_count)
+            curr_track_listeners = []
 
         # Set the current track_id as the previous track_id for next iteration
         prev_track = curr_track
 
-    # Print the current track's listen count
-    print_result(curr_track, curr_artist, curr_user_id, curr_user_first_name, curr_user_last_name, curr_track_count)
+    # Print the current track's listeners
+    for listener in curr_track_listeners:
+        print_result(prev_track, curr_artist, listener[0], listener[1], listener[2], listener[3])
 
 # Print track_id, artist, user_id, first_name, last_name, listen_count
 def print_result(track_id, artist, user_id, first_name, last_name, listen_count):

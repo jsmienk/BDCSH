efab0c91d4d7c24755451a435629f7a17bc101c3
Jeroen Smienk <jeroensmienk@icloud.com>
2018-09-17 14:14:47 +0200

    Bug fix.

--- assigment1.1/favourite_artist/reducer2.py
+++ assigment1.1/favourite_artist/reducer2.py
@@ -7,48 +7,46 @@ import sys
 # Reducer prints track_id, artist, user_id, first_name, last_name, listen_count
 
 def reducer():
-
     prev_track = None
     curr_track = None
-    curr_artist = None
-    curr_track_listeners = []
+    prev_artist = None
+    prev_track_listeners = []
 
-    # Input comes from STDIN: track_id, artist, user_id, first_name, last_name, listen_count
     for line in sys.stdin:
         data = line.strip().split(',')
 
         if len(data) != 6:
             continue
 
-        curr_track, artist, user_id, first_name, last_name, listen_count = data
+        curr_track, curr_artist, user_id, first_name, last_name, listen_count = data
 
         # Check argument type
         if not listen_count.isdigit():
             continue
 
-        # Append a user to the listeners list
-        if user_id != '-':
-            curr_track_listeners.append([user_id, first_name, last_name, listen_count])
-
         # If current track_id does not equal previous track_id
         if prev_track and prev_track != curr_track:
 
             # Print the previous track's listeners
-            print_result(prev_track, curr_artist, curr_track_listeners)
+            print_result(prev_track, prev_artist, prev_track_listeners)
 
             # Reset variables
-            curr_artist = None
-            curr_track_listeners = []
+            prev_artist = None
+            prev_track_listeners = []
 
         # Save the artist of the current track_id
-        if curr_artist == None and artist != '-':
-            curr_artist = artist
+        if curr_artist != '-':
+            prev_artist = curr_artist
+
+        # Append a user to the listeners list
+        if user_id != '-':
+            prev_track_listeners.append([user_id, first_name, last_name, listen_count])
 
         # Set the current track_id as the previous track_id for next iteration
         prev_track = curr_track
 
     # Print the current track's listeners
-    print_result(prev_track, curr_artist, curr_track_listeners)
+    print_result(prev_track, prev_artist, prev_track_listeners)
 
 # Print track_id, artist, user_id, first_name, last_name, listen_count
 def print_result(track_id, artist, listeners):

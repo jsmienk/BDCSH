9adb211d0e1e44102259b47ab002ac29fdf30a55
MarnickvdA <marnick@vdarend.com>
2018-09-20 13:31:44 +0200

    Assignment 1.3.1.

--- assignment1.3/ip_hit_administration/IPMapper.java
+++ assignment1.3/ip_hit_administration/IPMapper.java
@@ -2,18 +2,20 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 
 class IPMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        final Text work = new Text(((FileSplit) context.getInputSplit()).getPath().getName());
         final String line = value.toString();
 
-        for (final String word : line.split("\\W+")) {
-            context.write(new Text(word), new IntWritable(1));
+        String[] partials = line.split("\\W+");
+
+        if (partials.length > 0) {
+            Text ip = new Text(partials[0]);
+
+            context.write(ip, new IntWritable(1));
         }
     }
 }

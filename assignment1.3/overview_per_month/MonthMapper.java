333b58bdbb8f1e4942828d9870c717a7056ee12f
MarnickvdA <marnick@vdarend.com>
2018-09-21 13:16:03 +0200

    Error handling

--- assignment1.3/overview_per_month/MonthMapper.java
+++ assignment1.3/overview_per_month/MonthMapper.java
@@ -35,10 +35,9 @@ class MonthMapper extends Mapper<LongWritable, Text, IntWritable, IPOccurrence> 
 
                 // Write output
                 context.write(month, ipCount);
-            } catch (ParseException pex) {
+            } catch (IOException | ParseException e) {
                 // SDF does not match real date string
-                System.err.println(pex.getMessage());
-                System.err.println(line);
+                throw new IOException("Line: " + line + "\nMessage: " + e.getMessage());
             }
         }
     }
